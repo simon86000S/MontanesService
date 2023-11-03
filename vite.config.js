b6c79e97f580f8558b39906baefbe27870e8a429
simon <simondu06730@hotmail.fr>
2023-11-03 23:18:40 +0100

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'https://simon86000s.github.io/MontanesService',
+  base:'/MontanesService',
   plugins: [react()],
 })
+
